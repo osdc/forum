1d1fbfc6c41e7f27dc06f26b1833f87d4422def9
vihaanraina6 <305062048+vihaanraina6@users.noreply.github.com>
2026-08-20 13:30:44 +0530

    feat: add header component and set up project css - Add header component with organization logo, theme toggle, and (non-functional) notification and profile buttons - Add global css variables in base.css file for inheritance by components - Add individual css files for each component - Add tabler@icons - Ensure dark theme compatibility of all components, icons and svg

---
+++ frontend/src/js/components/header.js
@@ -0,0 +1,35 @@
+import './themeToggle.js';
+import './searchBar.js';
+import logo from '../../assets/osdc_logo.svg?raw';
+import bellFilledIcon from '@tabler/icons/filled/bell.svg?raw';
+
+
+// Import component CSS as a raw string using Vite's ?inline
+import stylesString from '../../css/components/header.css?inline';
+
+// Parse into a constructable stylesheet
+const headerStyles = new CSSStyleSheet();
+headerStyles.replaceSync(stylesString);
+
+class SiteHeader extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.adoptedStyleSheets = [headerStyles];
+    this.shadowRoot.innerHTML = `
+      <header>
+        <a href="index.html" class="logo">
+          ${logo}
+        </a>
+        <search-bar></search-bar>
+        <div class="actions">
+          <button class="notif-btn">${bellFilledIcon}</button>
+          <theme-toggle></theme-toggle>
+          <button class="profile-btn">JD</button>
+        </div>
+      </header>
+    `;
+  }
+}
+
+customElements.define('site-header', SiteHeader);
